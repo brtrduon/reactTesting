611713684b210a0aa4f481dac52a70e05120520d
unknown <you@example.com>
2018-02-18 19:06:22 -0800

    connected stuff with other stuff

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,3 +1,11 @@
+import { SAVE_COMMENT } from '../actions/types';
+
 export default function(state = [], action) {
-    
+    switch(action.type) {
+        case SAVE_COMMENT:
+        return [ ...state, action.payload ];
+        // the line directly above this line is equivalent to writing:
+        // return [state.concat([action.payload])];
+    }
+    return state;
 }
--- test/reducers/comments_test.js
+++ test/reducers/comments_test.js
@@ -4,11 +4,15 @@ import { SAVE_COMMENT } from '../../src/actions/types';
 
 describe('Comments Reducer', () => {
     it('handles action with unknown type', () => {
-        expect(commentReducer()).to.be.instanceof(Array);
-        expect(commentReducer()).to.eql([]);
+        // expect(commentReducer()).to.be.instanceof(Array);
+        expect(commentReducer(undefined, {})).to.eql([]);
+        // note: undefined is NOT the same as null
     });
 
     it('handles action of type SAVE_COMMENT', () => {
-
+        const action = { type: SAVE_COMMENT, payload: 'new comment' }
+        // whenever we handle a reducer test, we create the action ahead of time and then pass it into the reducer with some default state
+        // we expect that whatever gets returned from the reducer is the expected state
+        expect(commentReducer([], action)).to.eql(['new comment']);
     });
 });
