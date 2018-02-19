6e004ee36a35b742eaffe0dedb7f3289c5a8b050
unknown <you@example.com>
2018-02-18 17:20:17 -0800

    fixed a minor bug that prevented app from compiling

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { connect } from 'react';
+import { connect } from 'react-redux';
 import * as actions from '../actions';
 // we are saving every action creator in the variable 'actions'
 // '*' means everything / all
